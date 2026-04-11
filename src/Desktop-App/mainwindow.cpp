0535a9e2a6c3db039c2ccdb93b47c43e22246589
SMaready <stephan.maready@gmail.com>
2026-04-11 12:25:56 -0700

    Added pages to stackedWidget for corresponding buttons in navbar.

--- src/Desktop-App/mainwindow.cpp
+++ src/Desktop-App/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -9,6 +10,19 @@ MainWindow::MainWindow(QWidget *parent)
 
     //Set window size to take up whole screen with borders
     this->setWindowState(Qt::WindowMaximized);
+
+    connect(ui->dashboardPushButton, &QPushButton::clicked, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->dashboardPage);
+        std::cout << "Dashboard Page" << std::endl;
+    });
+    connect(ui->catalogPushButton, &QPushButton::clicked, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->catalogPage);
+        std::cout << "Catalog Page" << std::endl;
+    });
+    connect(ui->managePushButton, &QPushButton::clicked, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->managePage);
+        std::cout << "Manage Page" << std::endl;
+    });
 }
 
 MainWindow::~MainWindow()
